a40e13a179860f3aa556aa41c7e0f7ec002b3704
Tatyana404 <kot.kate2020@gmail.com>
2021-01-15 14:00:05 +0200

    Add range validator

---
+++ index.js
@@ -0,0 +1,62 @@
+"use strict";
+
+// Реализовать класс RangeValidator, со следующими свойствами:
+//     ■  from (number);
+//     ■  to (number);
+
+class RangeValidator {
+  /**
+   *
+   * @param {number} from
+   * @param {number} to
+   */
+  constructor(from, to) {
+    if (from >= to) {
+      throw new RangeError(
+        "The first number cannot be greater than the second"
+      );
+    }
+    this._from = from;
+    this._to = to;
+  }
+
+  // Реализовать getter'ы и setter'ы для обоих свойств
+
+  set from(newFrom) {
+    if (typeof newFrom !== "number") {
+      throw new TypeError("Parameters have unappropriate type");
+    }
+    this._from = newFrom;
+  }
+  get from() {
+    return this._from;
+  }
+  set to(newTo) {
+    if (typeof newTo !== "number") {
+      throw new TypeError("Parameters have unappropriate type");
+    }
+    this._to = newTo;
+  }
+  get to() {
+    return this._to;
+  }
+
+  // Реализовать getter range, который будет возвращать массив с двумя числами диапазона
+
+  get range() {
+    return [this._from, this._to];
+  }
+
+  // Реализовать метод validate, который будет принимать число и проверить входит ли число в указанный диапазон.
+
+  validate(num) {
+    if (typeof num !== "number") {
+      throw new TypeError("Parameters have unappropriate type");
+    }
+    if (num >= this._from && num <= this._to) {
+      return num;
+    } else {
+      throw new RangeError("Out of range");
+    }
+  }
+}
